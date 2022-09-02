8f967b34215da7058ac897a753cc53e797de83d2
Zhiqiang Wang <zhiwang20@yahoo.com>
2022-09-02 00:13:38 -0400

    setting up the initial project

---
+++ README.md
@@ -0,0 +1,23 @@
+# Installation Steps
+
+## Using npm
+
+Run commands
+
+1. `npm install`
+
+2. `npm run dev`
+
+## Or using yarn
+
+Run commands
+
+1. `npm install --global yarn`
+
+2. `yarn install`
+
+3. `yarn run dev`
+
+## Features
+
+- Next.js / Tailwind CSS / Redux / Firebase / NextAuth
---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
+      <center className="grid place-items-center mt-10">
+        <h1 className="text-5xl">Lets build an Amazon clone</h1>
+        <h2>This is your starter template!</h2>
+        <br />
+        <h3 className="font-bold">
+          We will be using Next.js / Tailwind CSS / Redux / Firebase / NextAuth
+        </h3>
+      </center>
+      {/* ---- ---- */}
+    </div>
+  );
+}
