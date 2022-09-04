8e6ff12d9b6ad391808891860d97fff0edff1831
Zhiqiang Wang <zhiwang20@yahoo.com>
2022-09-04 16:15:11 -0400

    add header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -6,17 +7,8 @@ export default function Home() {
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-
-      {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
-      <center className="grid place-items-center mt-10">
-        <h1 className="text-5xl">Lets build an Amazon clone</h1>
-        <h2>This is your starter template!</h2>
-        <br />
-        <h3 className="font-bold">
-          We will be using Next.js / Tailwind CSS / Redux / Firebase / NextAuth
-        </h3>
-      </center>
-      {/* ---- ---- */}
+      {/* Header Component */}
+      <Header />
     </div>
   );
 }
