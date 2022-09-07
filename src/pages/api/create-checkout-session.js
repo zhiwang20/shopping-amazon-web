43fdeed623a82a9b1fa6a2e61ca654bc47cbba80
Zhiqiang Wang <zhiwang20@yahoo.com>
2022-09-06 21:10:02 -0400

    add success screen

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -4,34 +4,36 @@ export default async (req, res) => {
   const { items, email } = req.body;
 
   const transformedItems = items.map((item) => ({
+    quantity: 1,
     price_data: {
       currency: "USD",
-      product_data: {
-        images: [item.image],
-        name: item.title,
-      },
       unit_amount: item.price * 100,
+      product_data: {
+        name: item.title,
+        images: [item.image],
+        description: item.description,
+      },
     },
-    description: item.description,
-    quantity: 1,
   }));
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-    shipping_rates: ["shr_1LerCEKQxeGsJUUX3VfzUpLV"],
+    shipping_options: [
+      {
+        shipping_rate: "shr_1LerCEKQxeGsJUUX3VfzUpLV",
+      },
+    ],
     shipping_address_collection: {
-      allowed_countries: ["US", "CA"],
+      allowed_countries: ["US", "RO"],
     },
     line_items: transformedItems,
     mode: "payment",
-    success_url: `${process.env.HOST}/success`, //dont forge to change HOST when you're deploying
+    success_url: `${process.env.HOST}/success`,
     cancel_url: `${process.env.HOST}/checkout`,
     metadata: {
       email,
       images: JSON.stringify(items.map((item) => item.image)),
     },
   });
-
   res.status(200).json({ id: session.id });
-  console.log(session.id);
 };
