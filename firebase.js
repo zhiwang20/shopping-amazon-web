cf7e4690c2af018b2735de2643bf6ce24580900d
Zhiqiang Wang <zhiwang20@yahoo.com>
2022-09-06 21:16:16 -0400

    fix firebase

--- firebase.js
+++ firebase.js
@@ -9,3 +9,11 @@ const firebaseConfig = {
   messagingSenderId: "252798018631",
   appId: "1:252798018631:web:b0578497bd1379b917fd62",
 };
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+
+export default db;
